2c65f4f54618708fdfa479f22ad40fb97b071ce1
Sergio de Carvalho <scarvalhojr@gmail.com>
2014-02-16 15:24:22 +0000

    Implement main page and details views
    Add main page view listing all polls, and details view showing poll
    question and choices. Also define namespace for poll app.

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,17 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
 from polls.models import Poll
 
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
-    output = ', '.join([p.question for p in latest_poll_list])
-    return HttpResponse(output)
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/detail.html', {'poll': poll})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
